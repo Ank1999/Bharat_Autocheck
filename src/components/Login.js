6371f904ff744dd2cfda6e632a9bd2254275b614
Arjun <azala008@gmail.om>
2024-01-26 23:10:29 +0530

    Added login sign up with API

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,143 +1,212 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import React, { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
 
-export default function Login({navigation}) {
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [otp, setOtp] = useState(['', '', '', '']);
-    const [isOtpSent, setIsOtpSent] = useState(false);
+export default function SignUpSignIn({ navigation }) {
+  const [isSignInView, setIsSignInView] = useState(true); // Toggle between sign-in and sign-up view
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [otp, setOtp] = useState(["", "", "", ""]);
+  const [isOtpSent, setIsOtpSent] = useState(false);
 
-    const handleSendOrVerifyOtp = () => {
-        if (isOtpSent) {
-            // Verify OTP code
-            const otpCode = otp.join('');
-            console.log('Verify OTP: ', otpCode);
-            // Here you would verify the OTP against your backend
-            Alert.alert('OTP Verification', 'The OTP verification is successful!', [{ text: 'OK' }]);
-            // navigation.navigate('HomeScreen'); 
-            navigation.navigate('Main');
-        } else {
-            // Send OTP code
-            console.log('Send OTP to: ', phoneNumber);
-            // Here you would send the OTP to the user's phone number via your backend
-            setIsOtpSent(true);
-        }
-    };
+  // OTP input refs
+  const firstInputRef = useRef(null);
+  const secondInputRef = useRef(null);
+  const thirdInputRef = useRef(null);
+  const fourthInputRef = useRef(null);
+  const otpInputRefs = [
+    firstInputRef,
+    secondInputRef,
+    thirdInputRef,
+    fourthInputRef,
+  ];
 
-    // Create individual refs for each OTP input
-    const firstInputRef = useRef(null);
-    const secondInputRef = useRef(null);
-    const thirdInputRef = useRef(null);
-    const fourthInputRef = useRef(null);
+  const handleSignUpOrSignIn = () => {
+    if (isSignInView) {
+      // Sign-In logic
+      console.log("Sign-In with: ", email, password);
+      //TODO
+      // Verify email and password with backend
+      
+    //   fetch("http://localhost:8080/user/login", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       username: username,
+    //       password: password,
+    //     }),
+    //   })
+    //     .then((response) => {console.log(response); response.json();})
+    //     .then((data) => {
+    //       // Handle successful sign-up response
+    //       navigation.navigate("Main");
+    //     })
+    //     .catch((error) => {
+    //       // Handle sign-up error
+    //       alert("Sign-in Error:");
+    //       console.error("Sign-in Error:", error);
+    //       // You can display an error message to the user
+    //     });
+    } else {
+      // Sign-Up logic
+      if (password !== confirmPassword) {
+        Alert.alert("Error", "Passwords do not match");
+        return;
+      }
+      console.log("Sign-Up with: ", email, password, phoneNumber);
+      // Create user account in your backend
+      // Make a POST request to your sign-up endpoint using fetch
+      fetch("http://localhost:8080/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          mobileNo: phoneNumber,
+          password: password,
+        }),
+      })
+        .then((response) => {console.log(response); response.json();})
+        .then((data) => {
+          // Handle successful sign-up response
+          navigation.navigate("Main");
+        })
+        .catch((error) => {
+          // Handle sign-up error
+          alert("Sign-Up Error:");
+          console.error("Sign-Up Error:", error);
+          // You can display an error message to the user
+        });
+    }
+  };
 
-    // Array of refs for convenience in handling focus
-    const otpInputRefs = [firstInputRef, secondInputRef, thirdInputRef, fourthInputRef];
+  return (
+    <View style={styles.container}>
+      {!isSignInView && (
+        <>
+          <TextInput
+            style={styles.input}
+            placeholder="Name"
+            value={username}
+            onChangeText={setUsername}
+            keyboardType="email-address"
+          />
+        </>
+      )}
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
 
-    const handleOtpChange = (text, index) => {
-        const newOtp = [...otp];
-        newOtp[index] = text;
-        setOtp(newOtp);
-
-        // Focus next input on entering a digit
-        if (text && index < otp.length - 1) {
-            otpInputRefs[index + 1].current.focus();
-        }
-    };
-
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.label}>Enter your phone number</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Phone Number"
-                keyboardType="phone-pad"
-                value={phoneNumber}
-                onChangeText={setPhoneNumber}
-                editable={!isOtpSent} // Make it non-editable after sending OTP
-            />
-            {isOtpSent && (
-                <View style={styles.otpInputsContainer}>
-                    {otp.map((digit, index) => (
-                        <TextInput
-                            key={index}
-                            ref={otpInputRefs[index]}
-                            style={styles.otpInput}
-                            keyboardType="number-pad"
-                            maxLength={1}
-                            onChangeText={(text) => handleOtpChange(text, index)}
-                            value={digit}
-                        />
-                    ))}
-                </View>
-            )}
-            <TouchableOpacity style={styles.button} onPress={handleSendOrVerifyOtp}>
-                <Text style={styles.buttonText}>{isOtpSent ? 'Verify OTP' : 'Send OTP'}</Text>
-            </TouchableOpacity>
-        </View>
-    );
-};
+      {!isSignInView && (
+        <>
+          <TextInput
+            style={styles.input}
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Phone Number"
+            keyboardType="phone-pad"
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+            editable={!isOtpSent}
+          />
+        </>
+      )}
+      <TouchableOpacity style={styles.button} onPress={handleSignUpOrSignIn}>
+        <Text style={styles.buttonText}>
+          {isSignInView ? "Sign In" : "Sign Up"}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.toggleButton}
+        onPress={() => setIsSignInView(!isSignInView)}
+      >
+        <Text style={styles.buttonText}>
+          {isSignInView ? "New here?  Create Account" : "Switch to Sign In"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    otpInputsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 20,
-        marginBottom: 20,
-    },
-    otpInput: {
-        width: '22%',
-        borderWidth: 1,
-        borderColor: '#ddd',
-        padding: 15,
-        fontSize: 18,
-        textAlign: 'center',
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        padding: 20,
-        backgroundColor: '#fff',
-    },
-    label: {
-        fontSize: 18,
-        marginBottom: 10,
-    },
-    input: {
-        fontSize: 18,
-        borderWidth: 1,
-        borderColor: '#ddd',
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginBottom: 10, // You can reduce this value if needed
-    },
-    otpInputsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 20,
-        marginTop: 10, // Reduced the margin at the top of OTP inputs
-        marginBottom: 20, // Adjust as needed
-    },
-    otpInput: {
-        width: '22%',
-        borderWidth: 1,
-        borderColor: '#ddd',
-        padding: 15,
-        fontSize: 18,
-        textAlign: 'center',
-        borderRadius: 15,
-
-    },
-    button: {
-        backgroundColor: '#34C759',
-        padding: 15,
-        borderRadius: 5,
-        alignItems: 'center',
-        marginTop: 20, // Adjust this margin to control space above the button
-    },
-    buttonText: {
-        fontSize: 18,
-        color: '#fff',
-        fontWeight: 'bold',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  toggleButton: {
+    alignSelf: "center",
+    marginTop: 30,
+    backgroundColor: "#007AFF", // Professional color
+    borderRadius: 8, // Rounded corners
+    paddingVertical: 12, // Vertical padding
+    paddingHorizontal: 24, // Horizontal padding
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  otpInputsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  otpInput: {
+    width: "22%",
+    borderWidth: 1,
+    borderColor: "#ddd",
+    padding: 15,
+    fontSize: 18,
+    textAlign: "center",
+    borderRadius: 15,
+  },
+  button: {
+    backgroundColor: "#34C759",
+    padding: 15,
+    borderRadius: 5,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#fff",
+    // fontWeight: 'bold',
+  },
 });

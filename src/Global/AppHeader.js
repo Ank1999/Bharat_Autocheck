27d1e83f2153c1eaef1a80820a381b4e54faf277
Ankit Kumar Singh <ank@Ankits-MacBook-Pro.local>
2024-03-13 18:57:45 +0530

    location serives aadded

--- src/Global/AppHeader.js
+++ src/Global/AppHeader.js
@@ -1,94 +1,64 @@
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView, StyleSheet } from 'react-native';
 import React from 'react'
 import bike from '../../assets/Bike.png'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faUser, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Ionicons } from '@expo/vector-icons';
 
 
 
 
-const AppHeader = ({ title, showBackButton, showText, onBackButtonPress, onProfileClick }) => {
+const AppHeader = ({ title, showText, onBackButtonPress, }) => {
   return (
-    <View style={styles.headerBar}>
-      {showBackButton ? (
-        <View>
-          {showText ? (
-            <View style={styles.headerContent}>
-              <TouchableOpacity style={styles.button} onPress={onBackButtonPress}>
-                <Icon name="chevron-left" size={30} color="white" />
-              </TouchableOpacity>
-              <View style={styles.nonClickableText}>
-                <Text style={styles.headerTitle}>{title}</Text>
-              </View>
-            </View>
-          ) : (
-            <TouchableOpacity style={styles.button} onPress={onBackButtonPress}>
-              <Icon name="chevron-left" size={30} color="black" />
-            </TouchableOpacity>
-          )}
+    
+    <SafeAreaView style={styles.safeArea}>
+      
+      {showText ? (
+        <View style={styles.header}>
+          <TouchableOpacity  onPress={onBackButtonPress}>
+          <Ionicons name="arrow-back" size={24} color="black" />
+          </TouchableOpacity>
+          <View style={styles.nonClickableText}>
+            <Text style={styles.headerTitle}>{title}</Text>
+          </View>
         </View>
       ) : (
-        <>
-          <Text style={styles.headerTitle}>{title}</Text>
-          <TouchableOpacity style={styles.profileButton} onPress={onProfileClick}>
-            <FontAwesomeIcon icon={faUser} size={22} style={styles.profileIcon} />
-          </TouchableOpacity>
-        </>
+        <TouchableOpacity  onPress={onBackButtonPress}>
+          <Ionicons name="arrow-back" size={24} color="black" />
+        </TouchableOpacity>
       )}
-    </View>
+    </SafeAreaView>
 
   );
 };
 
 const styles = StyleSheet.create({
-  headerBar: { 
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 10,
-    height: 100,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#4a6fa5',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 3,
-    elevation: 20,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
+  safeArea: {
+    // flex: 1,
+    backgroundColor: 'white'
   },
-  headerContent: {
+  header: {
     flexDirection: 'row',
     alignItems: 'center',
-    top:20
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e2e2e2',
+    backgroundColor: 'white'
   },
   headerTitle: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: 'bold',
-    color:'white'
+    marginLeft: 24
   },
 
-  profileIcon: {
-    color: 'black',
-    margin: 10
-  },
-  nonClickableText: {
-    opacity: 0.7, // Adjust the opacity to make it appear non-clickable
-  },
   button: {
-    width: 45, 
-    height: 45, 
-    borderRadius: 10, 
+    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    borderWidth: 1, 
-    borderColor: 'white', 
+    borderWidth: 1,
+    borderColor: 'white',
     backgroundColor: 'transparent',
     right: 12
   }
--- src/components/LocationScreen.js
+++ src/components/LocationScreen.js
@@ -1,131 +1,220 @@
-import React, { useState } from 'react';
-import {
-    View,
-    StyleSheet,
-    TextInput,
-    FlatList,
-    TouchableOpacity,
-    Text,
-    Modal,
-    KeyboardAvoidingView,
-    TouchableWithoutFeedback,
-    ScrollView,
-    Platform
-} from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, TextInput, TouchableOpacity, Text, Modal, KeyboardAvoidingView, SafeAreaView } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { Ionicons } from '@expo/vector-icons';
 
 const LocationScreen = ({ visible, onClose, navigation }) => {
     const [searchQuery, setSearchQuery] = useState('');
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
 
-    // Placeholder data for recent and saved addresses
-    const recentSearches = ['Gera World of Joy', 'BJRD SPORTS'];
-    const savedAddresses = ['homew', 'hommmm', 'homes', 'OFFICE'];
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            });
+        })();
+    }, []);
+
+    const INDIA_REGION = {
+        latitude: 22.3511148,
+        longitude: 78.6677428,
+        latitudeDelta: 20,
+        longitudeDelta: 20,
+    };
+
+    const handleSearch = async () => {
+        console.log('Search for:', searchQuery);
+
+        let results = await Location.geocodeAsync(searchQuery);
+        if (results.length > 0) {
+            let { latitude, longitude } = results[0];
+            setLocation({
+                latitude: latitude,
+                longitude: longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+            });
+        }
+    };
+
+    const getCurrentLocation = async () => {
+        try {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({
+                accuracy: Location.Accuracy.BestForNavigation,
+            });
+            setLocation({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+            });
+        } catch (error) {
+            // Handle or log error
+            console.error(error);
+            setErrorMsg(error.message);
+        }
+    };
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+
 
     return (
-
-
         <Modal
             visible={visible}
             transparent={true}
             animationType="slide"
             onRequestClose={onClose}
         >
-            <KeyboardAvoidingView
-                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-                style={styles.container}
-            >
-                <TouchableWithoutFeedback onPress={onClose}>
-                    <View style={styles.modalContainer}>
-                        <ScrollView contentContainerStyle={styles.scrollViewContent}>
-                            <View style={styles.modalContent}>
-                                {/* Search Bar */}
-                                <TextInput
-                                    style={styles.searchBar}
-                                    placeholder="Address search e.g. Nilgiri's HSR"
-                                    onChangeText={setSearchQuery}
-                                    value={searchQuery}
-                                />
-
-                                {/* Current Location Button */}
-                                <TouchableOpacity style={styles.currentLocationButton}>
-                                    <Text>Use current location</Text>
-                                </TouchableOpacity>
-
-                                {/* Recent Searches */}
-                                <Text style={styles.sectionTitle}>Recent Searches</Text>
-                                <FlatList
-                                    data={recentSearches}
-                                    renderItem={({ item }) => <Text>{item}</Text>}
-                                    keyExtractor={(item, index) => index.toString()}
-                                />
-
-                                {/* Saved Addresses */}
-                                <Text style={styles.sectionTitle}>Saved Addresses</Text>
-                                <FlatList
-                                    data={savedAddresses}
-                                    renderItem={({ item }) => <Text>{item}</Text>}
-                                    keyExtractor={(item, index) => index.toString()}
-                                />
-
-
-                                {/* <MapView
-                                    style={styles.map}
-                                   
-                                    initialRegion={{
-                                        latitude: 20.5937,
-                                        longitude: 78.9629,
-                                        latitudeDelta: 22.0,
-                                        longitudeDelta: 22.0,
-                                    }}
-                                /> */}
-                            </View>
-                        </ScrollView>
+            <KeyboardAvoidingView behavior="padding" style={styles.container}>
+                <SafeAreaView style={styles.safeArea}>
+                    <View style={styles.header}>
+                        <TouchableOpacity onPress={onClose}>
+                            <Ionicons name="arrow-back" size={24} color="black" />
+                        </TouchableOpacity>
+                        <Text style={styles.headerTitle}>Set delivery location</Text>
                     </View>
-                </TouchableWithoutFeedback>
+                    <View style={styles.searchSection}>
+                        <View style={styles.searchBarContainer}>
+                            <TextInput
+                                style={styles.searchBar}
+                                placeholder="Address search e.g. Nilgiri's HSR"
+                                onChangeText={setSearchQuery}
+                                value={searchQuery}
+                            />
+                            <Ionicons name="ios-search" size={20} color="#a3a3a3" style={styles.searchIcon} />
+                        </View>
+                        <View style={styles.dividerContainer}>
+                            <View style={styles.line} />
+                            <Text style={styles.orText}>OR</Text>
+                            <View style={styles.line} />
+                        </View>
+                        <TouchableOpacity onPress={getCurrentLocation} style={styles.currentLocationContainer}>
+                            <Ionicons name="ios-locate" size={24} color="#4CAF50" style={styles.locationIcon} />
+                            <Text style={styles.currentLocationText}>Use current location</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.mapContainer}>
+                        <MapView
+                            style={styles.map}
+                            initialRegion={INDIA_REGION}
+                            region={location}
+                        >
+                            {location && <Marker coordinate={location} />}
+                        </MapView>
+                    </View>
+                </SafeAreaView>
             </KeyboardAvoidingView>
         </Modal>
     );
 };
 
 const styles = StyleSheet.create({
-
     container: {
-        flex: 1,
-    },
-    modalContainer: {
         flex: 1,
         backgroundColor: 'rgba(0,0,0,0.5)',
     },
-    scrollViewContent: {
-        flexGrow: 1,
-        justifyContent: 'flex-end',
-        // alignItems: 'center',
+    safeArea: {
+        flex: 1,
+        backgroundColor: 'white'
     },
-    modalContent: {
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
+    header: {
+        flexDirection: 'row',
         alignItems: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: '#e2e2e2',
+        backgroundColor: 'white'
+    },
+    headerTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginLeft: 24
+    },
+    searchSection: {
+        backgroundColor: 'white',
+        paddingHorizontal: 16,
+        paddingTop: 16,
+    },
+    searchBarContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#F2F3F5',
+        borderRadius: 10,
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        marginBottom: 16,
     },
     searchBar: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        margin: 10,
-        borderRadius: 5,
-        padding: 10,
-    },
-    currentLocationButton: {
-        margin: 10,
-    },
-    sectionTitle: {
-        fontWeight: 'bold',
+        flex: 1,
+        paddingVertical: 8,
+        paddingHorizontal: 10,
         fontSize: 16,
-        marginTop: 10,
-        marginLeft: 10,
+        color: '#424242',
+    },
+    searchIcon: {
+        marginLeft: 8,
+    },
+    dividerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 16,
+    },
+    line: {
+        flex: 1,
+        height: 1,
+        backgroundColor: '#E0E0E0',
+    },
+    orText: {
+        marginHorizontal: 16,
+        color: '#9E9E9E',
+    },
+    currentLocationContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    locationIcon: {
+        marginRight: 8,
+    },
+    currentLocationText: {
+        fontSize: 16,
+        color: '#4CAF50',
+        textDecorationLine: 'none',
+    },
+    mapContainer: {
+        flex: 1,
+        paddingHorizontal: 16,
+        paddingVertical: 16,
     },
     map: {
-        ...StyleSheet.absoluteFillObject,
+        height: 200,
+        borderRadius: 8,
+        // marginHorizontal: 16,
+        marginBottom: 16,
     },
 });
 
